030eb66c1f298f317237a3728729e311293e5988
pavlov200912 <pavlov200912@gmail.com>
2019-10-15 22:40:03 +0300

    data writer modified in case of witkionary

--- data/data_writer.py
+++ data/data_writer.py
@@ -13,7 +13,23 @@ class DataWriter:
         self._file = open(self._path, 'w+') # TODO w+?, exceptions!
         self._writer = csv.writer(self._file, delimiter='\\')
 
-    def write(self, title, text):
+    def write(self, data, is_wikipedia):
+        try:
+            title, title_data = data[0], data[1]
+        except ValueError:
+            return
+        if not title or not title_data:
+            return
         if not self._file_open:
             self._open_file()
-        self._writer.writerow([title, text])
+        if is_wikipedia:
+            self._writer.writerow([title, title_data])
+        else:
+            try:
+                part_of_speech = title_data['part of speech']
+                meanings = title_data['meanings']
+                relations = title_data['relations']
+                pharaseme = title_data['phraseme']
+            except KeyError:
+                return
+            self._writer.writerow([title, part_of_speech, meanings, relations, pharaseme])
