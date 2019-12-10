426c07bd895925df690493e6168ca0ce10ff85af
pavlov200912 <pavlov200912@gmail.com>
2019-12-10 16:03:10 +0000

    tests implemented

---
+++ bot/test.py
@@ -0,0 +1,21 @@
+import script
+import pandas as pd
+m = script.KontaktModel()
+df = pd.read_csv('wide_wikt_means.csv')
+
+global_success = 0
+
+def test():
+    global global_success
+    sample = df.sample(1).iloc[0]
+    size = min(1, len(sample.title))
+    answer = m.predict_word(sample.meaning, sample.title[:size])
+    print(f'Word: {sample.title}; Answer: {answer}')
+    if answer == sample.title:
+        global_success += 1
+
+
+for i in range(0, 1000):
+    test()
+
+print(f'Total accuracy: {global_success / 100}')
