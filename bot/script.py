4683959666d0ae4baae7c467f1286f02acbc8573
pavlov200912 <pavlov200912@gmail.com>
2019-12-10 21:21:55 +0300

    preprocessing added

--- bot/script.py
+++ bot/script.py
@@ -8,19 +8,33 @@ from ufal.udpipe import Model, Pipeline
 import pandas as pd
 import numpy as np
 import random
+import nltk
+from nltk.corpus import stopwords
+from pymystem3 import Mystem
+from string import punctuation
 
 
 class KontaktModel:
     def __init__(self):
         standard_library.install_aliases()
+        nltk.download('stopwords')
+        mystem = Mystem()
+        russian_stopwords = stopwords.words("russian")
+
         # colujmns: title
         self.nouns = pd.read_csv('wide_wikt.csv')
         self.local_nouns = None  # TODO: Add locality
         self.model = gensim.models.KeyedVectors.load('araneum_none_fasttextcbow_300_5_2018.model')
 
-    def predict_word(self, text, prefix='а'):
-        # TODO: ADD TEXT LEMMATIZATION
-        words = text.split(' ')
+    def preprocess_text(self, text):
+        tokens = mystem.lemmatize(text.lower())
+        tokens = [token for token in tokens if token not in russian_stopwords \
+                  and token != " " \
+                  and token.strip() not in punctuation]
+        return " ".join(tokens)
+
+    def predict_word(self, text, prefix=''):
+        words = (self.preprocess_text(text)).split(' ')
         prefix_titles = self.nouns[self.nouns['title'].str.startswith(prefix)]
 
         if prefix_titles.empty:
